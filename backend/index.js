2382dda238a1dabfb2a62de84b353fe013fabd7f
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-10-03 13:59:52 +0200

    added the secon part of register and login form

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const bcrypt = require ('bcryptjs');
 const User = require('./models/User');
 
 
@@ -13,6 +14,8 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
 
 const jwtSecret = process.env.JWT_SECRET;
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 
@@ -43,9 +46,28 @@ app.get('/profile', (request,response) => {
   }
 });
 
+app.post('/login', async (request,response) => {
+  const {username, password} = request.body;
+  const foundUser = await User.findOne({username});
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
+        response.cookie('token', token, {sameSite:'none', secure:true}).json({
+          id: foundUser._id,
+        });
+      });
+    }
+  } 
+});
+
 app.post('/register', async (request,response) => {
   const {username,password} = request.body;
-  const createdUser = await User.create({username,password});
+  const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
+  const createdUser = await User.create({
+    username:username,
+    password:hashedPassword,
+  });
   jwt.sign({userId:createdUser._id,username}, jwtSecret, {}, (err, token) => {
     if (err) throw err;
     response.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
