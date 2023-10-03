72653cc432d870532818b226ecd2bb543658559c
elvisguybakunzi <e.bakunzi@alustudent.com>
2023-10-03 19:03:26 +0200

    added the frontend of chat space

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import Register from "./RegisterAndLoginForm";
 import { UserContext } from "./UserContext";
+import Chat from "./Chat";
 
 export default function Routes() {
   // eslint-disable-next-line no-unused-vars
   const {username, id} =useContext(UserContext);
 
   if (username) {
-    return 'Logged in ' + username;
+   return <Chat />;
   }
 
   return (
